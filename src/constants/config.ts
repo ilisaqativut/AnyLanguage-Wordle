77b40d809318ad1d35940600bd9c58f579084db2
ilisaqativut <84241244+ilisaqativut@users.noreply.github.com>
2022-02-08 09:15:28 -0800

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'Iñupiaq (NS)', // This changes the display name for your language
+  language: 'Iñupiaq', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Iḷisaqativut', // Put your name here so people know who made this Wordle!
   authorWebsite: 'https://ilisaqativut.org/', // Put a link to your website or social media here
